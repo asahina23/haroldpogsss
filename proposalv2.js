02bac8370eaf43fd8d991cdae4c21dd7cf0ac317
Harold Vinoya <111508176+asahina23@users.noreply.github.com>
2023-10-05 16:03:59 +0800

    Update proposalv2.js

--- proposalv2.js
+++ proposalv2.js
@@ -39,7 +39,7 @@ const hahahuhu = document.getElementById("hahahuhu");
 const hang = document.getElementById("hang");
 const sadsad = document.getElementById("sadsad");
 const frog = document.getElementById("frog");
-const frog = document.getElementById("sad23");
+const sad23 = document.getElementById("sad23");
 
 no.addEventListener("click", function () {
   btt12.style.opacity = "1";
